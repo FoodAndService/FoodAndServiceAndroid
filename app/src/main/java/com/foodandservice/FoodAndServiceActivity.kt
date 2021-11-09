a548ccee13e44b0a4e9ed4e480e191b68bc71b84
kingb1rd <kingb1rdx@gmail.com>
2021-11-09 17:54:12 +0100

    feat: bottom bar navigation + qr scan button design

--- app/src/main/java/com/foodandservice/FoodAndServiceActivity.kt
+++ app/src/main/java/com/foodandservice/FoodAndServiceActivity.kt
@@ -2,10 +2,17 @@ package com.foodandservice
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.foodandservice.databinding.ActivityFoodandserviceBinding
 
 class FoodAndServiceActivity : AppCompatActivity() {
+    private lateinit var databinding: ActivityFoodandserviceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_foodandservice)
+        databinding = DataBindingUtil.setContentView(this, R.layout.activity_foodandservice)
+        databinding.bottomNavView.setupWithNavController(findNavController(R.id.navHostFragment))
     }
 }
